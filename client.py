6ed05bf744260d2ea16ed1b8443ab3d5d05ee55a
Koen Molenaar <koen.molenaar26@gmail.com>
2021-03-16 13:22:15 +0100

    werkende infinite client

--- client.py
+++ client.py
@@ -1,5 +1,15 @@
 import http.client as client
+from time import sleep
 
 con = client.HTTPConnection('127.0.0.1' , 8000)
 
-con.request("GET", "/test")
+try:
+    while True:
+        con.request("GET", "/test")
+        res = con.getresponse()
+        print(res.getheaders())
+        sleep(2.5)
+except KeyboardInterrupt:
+    con.close()
+    print("Closing...")
+
