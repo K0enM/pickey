030be98ada9468f4a2bf3ac5030d53b186e34dfd
Koen Molenaar <koen.molenaar26@gmail.com>
2021-03-17 15:49:36 +0100

    Vier werkende sensoren

--- client.py
+++ client.py
@@ -1,11 +1,11 @@
 import http.client as client
 from time import sleep
 
-con = client.HTTPConnection('127.0.0.1' , 8000)
+con = client.HTTPConnection('mod7-pickey.herokuapp.com' , 80)
 
 try:
     while True:
-        con.request("GET", "/test")
+        con.request("GET", "/")
         res = con.getresponse()
         print(res.getheaders())
         sleep(2.5)
--- hanger.py
+++ hanger.py
@@ -6,7 +6,6 @@ from adafruit_ads1x15.analog_in import AnalogIn
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN)
 
 # setup i2c bus
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -15,11 +14,17 @@ i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1115(i2c)
 
 chan0 = AnalogIn(ads, ADS.P0)
+chan1 = AnalogIn(ads, ADS.P1)
+chan2 = AnalogIn(ads, ADS.P2)
+chan3 = AnalogIn(ads, ADS.P3)
 
 try:
     while True:
-        print("{:>5.3f}".format(chan0.voltage))
-        sleep(2.5)
+        print("Sensor 0: {:>5.3f}".format(chan0.voltage))
+        print("Sensor 1: {:>5.3f}".format(chan1.voltage))
+        print("Sensor 2: {:>5.3f}".format(chan2.voltage))
+        print("Sensor 3: {:>5.3f}".format(chan3.voltage))
+        sleep(0.1)
 except KeyboardInterrupt:
     GPIO.cleanup()
     print("Ending...")
